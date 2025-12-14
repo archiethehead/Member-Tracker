008a116508e530a9543512cacf09c4a2c24f6aed
Archie Healy <archiethehead@gmail.com>
2025-12-14 15:40:30 +0000

    Check for New and Missing Members
    Feature: The bot can now check if there is a new member in the server. If one or more are detected, they're appended to the member file.
    
    Feature: The bot can now check if a member is missing from the server, and alerts in general if this is the case.
    
    Fix: Changed all instances of .write() to print().

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from os import remove
 
 def write_to_client_list(new_client):
     client_file = open("client_file.txt", "a")
-    client_file.write(new_client, '\n')
+    print(new_client, file = client_file)
 
-def remove_client(client_list):
+def remove_client():
     remove("client_file.txt")
-    create_client_list()
 
 def create_client_list(client_list):
     new_client_file = open("client_file.txt", "a")
@@ -29,8 +28,44 @@ def get_client_list():
     except FileNotFoundError:
         return []
 
-client_list = get_client_list()
+def check_match(discord_client_list, client_list):
 
+    missing_clients = []
+    for member in client_list:
+        found = False
+
+        for discord_member in discord_client_list:
+            if member == discord_member:
+                found = True
+
+        if found == False:
+            missing_clients.append(member)
+    
+    return missing_clients
+
+def check_new_clients(discord_client_list, client_list):
+
+    missing_clients = []
+
+    for discord_member in discord_client_list:
+        found = False
+
+        for member in client_list:
+            if discord_member == member:
+                found = True
+
+        if found == False:
+            missing_clients.append(discord_member)
+    
+    return missing_clients
+
+def get_discord_list():
+    discord_client_list = []
+    for guild in client.guilds:
+        channel = discord.utils.get(guild.text_channels, name="general")
+        for member in guild.members:
+            discord_client_list.append(str(member.id))
+    return channel, discord_client_list
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -41,11 +76,32 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+    client_list = get_client_list()
+    channel, discord_client_list = get_discord_list()
+
     if client_list == []:
-        for guild in client.guilds:
-            for member in guild.members:
-                client_list.append(member.name)
+        client_list = discord_client_list
         create_client_list(client_list)
-    print(client_list)
+        client_list = get_client_list()
+    
+    new_clients = check_new_clients(discord_client_list, client_list)
+    if (len(new_clients) != 0):
+        for member in new_clients:
+            write_to_client_list(member)
+        client_list = get_client_list()
+
+    missing_clients = check_match(discord_client_list, client_list)
+    if (len(missing_clients) != 0):
+
+        if channel:
+            for member in missing_clients:
+                user = await client.fetch_user(member)
+                username = user.global_name or user.name
+                string = (str(username) + " has left the server!")
+                await channel.send(string)
+
+        remove_client()
+        create_client_list(discord_client_list)
+        client_list = get_client_list()
 
 client.run(getenv("DISCORD_TOKEN"))
