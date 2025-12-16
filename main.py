a777df17b887959e16178691ad67f666598d3835
Archie Healy <archiethehead@gmail.com>
2025-12-16 14:04:07 +0000

    OOP
    Fix: The higher bot functionality is now abstracted in an Object Oriented format, being interacted with from within on_ready.
    
    Fix: One function (check_array_match) to determine missing and new members.

--- main.py
+++ main.py
@@ -3,108 +3,102 @@ from os import getenv
 from os import remove
 from os import _exit
 
-def write_to_client_list(new_client):
-    client_file = open("client_file.txt", "a")
-    print(new_client, file = client_file)
+class member_tracker():
 
-def remove_client():
-    remove("client_file.txt")
+    def __init__(self):
+        self.bot_client = self.get_client()
+        self.discord_client_list = None
+        self.client_list = self.get_client_list()
+    
+    def get_discord_client_list(self):
+        discord_client_list = []
 
-def create_client_list(client_list):
-    new_client_file = open("client_file.txt", "a")
-    for client in client_list:
-        print(client, file = new_client_file)
+        for guild in self.bot_client.guilds:
+            for member in guild.members:
+                discord_client_list.append(str(member.id))
 
-def get_client_list():
+        return discord_client_list
 
-    try:
-        client_file = open("client_file.txt", "r")
-        client_list =  client_file.readlines()
+    def get_client_list(self):
         
-        for x in range(len(client_list)):
-            client_list[x] = client_list[x].strip('\n')
+        try:
+            client_file = open("client_file.txt", "r")
+            client_list =  client_file.readlines()
+        
+            for x in range(len(client_list)):
+                client_list[x] = client_list[x].strip('\n')
 
-        return client_list
+            return client_list
 
-    except FileNotFoundError:
-        return []
+        except FileNotFoundError:
+            return []
+    
+    def create_client_list(self, client_list):
+        new_client_file = open("client_file.txt", "a")
+        for client in client_list:
+            print(client, file = new_client_file)
+    
+    
+    def write_to_client_list(self, new_client):
+        client_file = open("client_file.txt", "a")
+        print(new_client, file = client_file)
+    
+    def delete_client_list(self):
+        remove("client_file.txt")
+    
+    def get_client(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        intents.members = True
+        client = discord.Client(intents=intents)
+        return client
 
-def check_match(discord_client_list, client_list):
 
-    missing_clients = []
-    for member in client_list:
+def check_array_match(array_one, array_two):
+    
+    missing_elements = []
+
+    for asset in array_one:
         found = False
 
-        for discord_member in discord_client_list:
-            if member == discord_member:
+        for asset_two in array_two:
+
+            if asset == asset_two:
                 found = True
-
+            
         if found == False:
-            missing_clients.append(member)
+            missing_elements.append(asset)
     
-    return missing_clients
+    return missing_elements
 
-def check_new_clients(discord_client_list, client_list):
+member_tracker_bot = member_tracker()
 
-    missing_clients = []
-
-    for discord_member in discord_client_list:
-        found = False
-
-        for member in client_list:
-            if discord_member == member:
-                found = True
-
-        if found == False:
-            missing_clients.append(discord_member)
-    
-    return missing_clients
-
-def get_discord_list():
-    discord_client_list = []
-    for guild in client.guilds:
-        channel = discord.utils.get(guild.text_channels, name="general")
-        for member in guild.members:
-            discord_client_list.append(str(member.id))
-    return channel, discord_client_list
-
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
-client = discord.Client(intents=intents)
-
-@client.event
+@member_tracker_bot.bot_client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    member_tracker_bot.discord_client_list = member_tracker_bot.get_discord_client_list()
 
-    client_list = get_client_list()
-    channel, discord_client_list = get_discord_list()
+    if member_tracker_bot.client_list == []:
+        member_tracker_bot.client_list = member_tracker_bot.discord_client_list
+        member_tracker_bot.create_client_list()
+        member_tracker_bot.client_list = member_tracker_bot.get_client_list()
 
-    if client_list == []:
-        client_list = discord_client_list
-        create_client_list(client_list)
-        client_list = get_client_list()
-    
-    new_clients = check_new_clients(discord_client_list, client_list)
-    if (len(new_clients) != 0):
-        for member in new_clients:
-            write_to_client_list(member)
-        client_list = get_client_list()
+    new_clients = check_array_match(member_tracker_bot.discord_client_list, member_tracker_bot.client_list)
+    for member in new_clients:
+        member_tracker_bot.write_to_client_list(member)
+    member_tracker_bot.client_list = member_tracker_bot.get_client_list()
 
-    missing_clients = check_match(discord_client_list, client_list)
-    if (len(missing_clients) != 0):
+    missing_clients = check_array_match(member_tracker_bot.client_list, member_tracker_bot.discord_client_list)
+    if len(missing_clients) != 0:
+        channel = await member_tracker_bot.bot_client.fetch_channel(getenv('CHANNEL_ID'))
+        for member in missing_clients:
+            user = await member_tracker_bot.bot_client.fetch_user(member)
+            username = user.global_name or user.name
+            await channel.send(f"{username} has left the server!")
 
-        if channel:
-            for member in missing_clients:
-                user = await client.fetch_user(member)
-                username = user.global_name or user.name
-                string = (str(username) + " has left the server!")
-                await channel.send(string)
+        member_tracker_bot.delete_client_list()
+        member_tracker_bot.create_client_list(member_tracker_bot.discord_client_list)
+        member_tracker_bot.client_list = member_tracker_bot.get_client_list()
 
-        remove_client()
-        create_client_list(discord_client_list)
-        client_list = get_client_list()
     _exit(1)
 
-
-client.run(getenv("DISCORD_TOKEN"))
+member_tracker_bot.bot_client.run(getenv("DISCORD_TOKEN"))
